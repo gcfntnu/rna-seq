50a5508a0d9d2c4bb3a840d961f7a7773fa9c947
flatberg <arnar.flatberg@ntnu.no>
2019-02-06 11:17:18 +0100

    F4

--- rules/db/scripts/gtf2tx.py
+++ rules/db/scripts/gtf2tx.py
@@ -1,11 +1,14 @@
 import sys
 from gtfparse import read_gtf
 
+ADD_TXVER = False
+
 df = read_gtf(sys.argv[1])
 df_tx = df[df['feature'] == 'transcript'].copy()
-tx = df_tx['transcript_id']
-tx_ver = df_tx['transcript_version']
-df_tx.loc[:,'transcript_id'] = ['{}.{}'.format(i, j) for i,j in zip(tx, tx_ver)]
+if ADD_TXVER:
+    tx = df_tx['transcript_id']
+    tx_ver = df_tx['transcript_version']
+    df_tx.loc[:,'transcript_id'] = ['{}.{}'.format(i, j) for i,j in zip(tx, tx_ver)]
 cols = df_tx.columns.copy()
 cols = cols.insert(0, 'transcript_id')
 df_tx = df_tx[cols]
